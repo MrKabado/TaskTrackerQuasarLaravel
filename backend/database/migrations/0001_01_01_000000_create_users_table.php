b4354283fac0c9df3cc40693a13691f488466efc
MrKabado <jersonjaybonghanoy@gmail.com>
2026-09-24 22:35:23 +0800

    chore: setup user model

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['user', 'admin'])->default('user');
+            $table->timestamp('last_login_at')->nullable();
+            $table->enum('account_status', ['active', 'deactivated'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
